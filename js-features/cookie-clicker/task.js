b639dc312d202ad3393f91652e2143036cdc9203
Subfokus <nkoleva@mail.ru>
2024-03-13 23:09:00 +0500

    first commit countdown

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,17 +1,11 @@
-
 const cookie = document.getElementById("cookie");
-const counter = document.getElementById("counter");
 
 let clicks = 0;
 
 cookie.addEventListener("click", () => {
   clicks++;
 
-  counter.textContent = clicks;
-
-  if (cookie.classList.contains("small")) {
-    cookie.classList.remove("small");
-  } else {
-    cookie.classList.add("small");
-  }
+  cookie.style.width = `${100 + clicks}px`;
+  
+  document.body.insertAdjacentHTML('beforeend', `<p>Количество кликов: ${clicks}</p>`);
 });
