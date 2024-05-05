45ffb413169d9a6a7ccec77476870dbc29c91d98
Subfokus <nkoleva@mail.ru>
2024-05-05 18:20:47 +0500

    first commit todo

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -3,8 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const addTaskButton = document.querySelector('#add-task-btn');
     const taskList = document.querySelector('.task-list');
 
-    if (!taskInput || !addTaskButton || !taskList) return;
-
     function addTask(title) {
         const taskHTML = `
             <div class="task">
@@ -15,19 +13,21 @@ document.addEventListener('DOMContentLoaded', function() {
         taskList.insertAdjacentHTML('afterbegin', taskHTML);
     }
 
-    addTaskButton.addEventListener('click', function(event) {
-        event.preventDefault();
-        const taskTitle = taskInput.value.trim();
-        if (taskTitle !== '') {
-            addTask(taskTitle);
-            taskInput.value = '';
-        }
-    });
-
-    taskList.addEventListener('click', function(event) {
-        if (event.target.classList.contains('task__remove')) {
+    if (taskInput && addTaskButton && taskList) {
+        addTaskButton.addEventListener('click', function(event) {
             event.preventDefault();
-            event.target.parentElement.remove();
-        }
-    });
+            const taskTitle = taskInput.value.trim();
+            if (taskTitle !== '') {
+                addTask(taskTitle);
+                taskInput.value = '';
+            }
+        });
+
+        taskList.addEventListener('click', function(event) {
+            if (event.target.classList.contains('task__remove')) {
+                event.preventDefault();
+                event.target.parentElement.remove();
+            }
+        });
+    }
 });
